09e0d4c2e1f0a43de9f610a64cdbb4b13dbfa690
Cem Basoglu <cem.basoglu@web.de>
2016-11-10 13:29:45 +0100

    Models updated

--- de.fhbielefeld.scl.KINewsBoard.DataLayer/src/de/fhbielefeld/scl/KINewsBoard/DataLayer/NewsBoardManager.java
+++ de.fhbielefeld.scl.KINewsBoard.DataLayer/src/de/fhbielefeld/scl/KINewsBoard/DataLayer/NewsBoardManager.java
@@ -1,22 +1,35 @@
 package de.fhbielefeld.scl.KINewsBoard.DataLayer;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceUnit;
+import javax.persistence.Persistence;
+import java.io.Closeable;
+import java.io.IOException;
 import java.util.List;
 
 /**
  * Created by cem on 10.11.16.
  */
-public class NewsBoardManager {
+public class NewsBoardManager implements Closeable {
 
-    @PersistenceUnit
     private EntityManager entityManager;
 
 
     public List<NewsEntry> getNewsEntries() {
+        EntityManager mngr = getEntityManager();
 
-        return entityManager.createNamedQuery("NewsEntry.findAll", NewsEntry.class).getResultList();
+        return mngr.createNamedQuery("NewsEntry.findAll", NewsEntry.class).getResultList();
     }
 
+    private EntityManager getEntityManager() {
+        if (entityManager == null)
+            entityManager = Persistence.createEntityManagerFactory("NewsBoardPU").createEntityManager();
 
+        return entityManager;
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (entityManager != null)
+            entityManager.close();
+    }
 }
