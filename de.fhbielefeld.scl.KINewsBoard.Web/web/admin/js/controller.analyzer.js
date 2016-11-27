9616aa8e7a849de75ae0a5922d9d4a0c32abcd95
Cem Basoglu <cem.basoglu@web.de>
2016-11-27 13:25:53 +0100

    CRUD Analyzer admin frontend done.

--- de.fhbielefeld.scl.KINewsBoard.Web/web/admin/js/controller.analyzer.js
+++ de.fhbielefeld.scl.KINewsBoard.Web/web/admin/js/controller.analyzer.js
@@ -10,9 +10,70 @@ angular.module('nwbadmin.analyzer', ['ui.router'])
             controller: 'AnalyzerViewController'
         });
 
+
     }])
     .controller('AnalyzerViewController',
-        ['$scope', '$location',
-            function ($scope, $location) {
+        ['$scope', '$location', 'AnalyzerService', 'GroupSetService', 'TokenService',
+            function ($scope, $location, AnalyzerService, GroupSetService, TokenService) {
 
-            }]);
+                $scope.update = function () {
+                    GroupSetService.getAll().then(function(data){
+                        $scope.groups = data;
+                    });
+                    AnalyzerService.getAll().then(function (data) {
+                        $scope.analyzers = data;
+                        $scope.active = null;
+                    });
+                };
+
+                $scope.select = function (analyzer) {
+                    $scope.active = angular.copy(analyzer);
+                };
+
+                $scope.delete = function () {
+                    if (!$scope.active) return;
+
+                    AnalyzerService.delete($scope.active.id).then(function (result) {
+                        $scope.update();
+                    });
+                };
+
+                $scope.save = function () {
+                    if (!$scope.active) return;
+
+                    if (!$scope.active.id) {
+                        AnalyzerService.create($scope.active).then(function (result) {
+                            $scope.update();
+                        });
+
+                    }
+                    else {
+                        AnalyzerService.update($scope.active).then(function (result) {
+                            $scope.update();
+                        });
+                    }
+                };
+
+                $scope.create = function () {
+                    $scope.active = {
+                        disabled: false,
+                        groups: [],
+                        name: "",
+                        token: TokenService.generate()
+                    };
+                };
+
+                $scope.toggle = function(group) {
+                    var idx = $scope.active.groups.indexOf(group);
+
+                    if (idx > -1) {
+                        $scope.active.groups.splice(idx, 1);
+                    }
+                    else {
+                        $scope.active.groups.push(group);
+                    }
+                }
+
+                $scope.update();
+            }
+        ]);
---
+++ de.fhbielefeld.scl.KINewsBoard.Web/web/admin/js/service.group.js
@@ -0,0 +1,37 @@
+angular.module('nwbadmin')
+    .factory('GroupSetService', ['$http', '$q', function ($http, $q) {
+        var api = "/WebService/backend/group/"
+        var service = {};
+
+        service.getAll = function () {
+            return $http.get(api).then(function (response) {
+                return response.data;
+            });
+        };
+
+        service.get = function (id) {
+            return $http.get(api + id).then(function (response) {
+                return response.data;
+            });
+        };
+
+        service.create = function (entity) {
+            return $http.post(api, entity).then(function (response) {
+                return true;
+            });
+        }
+
+        service.update = function (entity) {
+            return $http.put(api, entity).then(function (response) {
+                return true;
+            });
+        }
+
+        service.delete = function (id) {
+            return $http.delete(api + id).then(function (response) {
+                return true;
+            });
+        }
+
+        return service;
+    }]);
