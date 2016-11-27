2bfaeadd765566314113231d0ff95ab3877dcb93
Cem Basoglu <cem.basoglu@web.de>
2016-11-27 14:38:38 +0100

    CRUD groups admin frontend done

--- de.fhbielefeld.scl.KINewsBoard.Web/web/admin/js/controller.groups.js
+++ de.fhbielefeld.scl.KINewsBoard.Web/web/admin/js/controller.groups.js
@@ -7,12 +7,57 @@ angular.module('nwbadmin.groups', ['ui.router'])
         $stateProvider.state('groups', {
             url: '/groups',
             templateUrl: 'views/groups/index.html',
-            controller: 'GroupsViewController'
+            controller: 'GroupViewController'
         });
 
-    }])
-    .controller('GroupsViewController',
-        ['$scope', '$location',
-            function ($scope, $location) {
 
-            }]);
+    }])
+    .controller('GroupViewController',
+        ['$scope', '$location', 'GroupSetService',
+            function ($scope, $location, GroupSetService) {
+
+
+                $scope.update = function () {
+                    GroupSetService.getAll().then(function (data) {
+                        $scope.groups = data;
+                        $scope.active = null;
+                    });
+                };
+
+                $scope.select = function (group) {
+                    $scope.active = angular.copy(group);
+                };
+
+                $scope.delete = function () {
+                    if (!$scope.active) return;
+
+                    GroupSetService.delete($scope.active.id).then(function (result) {
+                        $scope.update();
+                    });
+                };
+
+                $scope.save = function () {
+                    if (!$scope.active) return;
+
+                    if (!$scope.active.id) {
+                        GroupSetService.create($scope.active).then(function (result) {
+                            $scope.update();
+                        });
+
+                    }
+                    else {
+                        GroupSetService.update($scope.active).then(function (result) {
+                            $scope.update();
+                        });
+                    }
+                };
+
+                $scope.create = function () {
+                    $scope.active = {
+                        name: ""
+                    };
+                }
+
+                $scope.update();
+            }
+        ]);
