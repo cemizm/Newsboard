cd71862d7dba6b962cc033536492980a5cb1a5d8
veroi4e4e4ka <w53455@student.wsiz.rzeszow.pl>
2016-12-01 19:14:34 +0100

    Add More Views

--- de.fhbielefeld.scl.KINewsBoard.Web/web/admin/js/controller.views.js
+++ de.fhbielefeld.scl.KINewsBoard.Web/web/admin/js/controller.views.js
@@ -9,10 +9,123 @@ angular.module('nwbadmin.views', ['ui.router'])
             templateUrl: 'views/views/index.html',
             controller: 'ViewsViewController'
         });
+        $stateProvider.state('editview', {
+            url: '/editviews',
+            templateUrl: 'views/views/edit.html',
+            controller: 'ViewsEditController'
+        });
 
     }])
-    .controller('ViewsViewController',
+    /*.controller('ViewsViewController',
         ['$scope', '$location',
             function ($scope, $location) {
 
-            }]);
+            }]);*/
+.controller('ViewsViewController',
+    ['$scope', '$location', 'ViewService', 'GroupSetService',
+        function ($scope, $location, ViewService, GroupSetService) {
+
+            $scope.update = function () {
+                GroupSetService.getAll().then(function (data) {
+                    $scope.groups = data;
+                });
+                ViewService.getAll().then(function (data) {
+                    $scope.views = data;
+                    $scope.active = null;
+                });
+            };
+
+            $scope.select = function (view) {
+                $scope.active = angular.copy(view);
+            };
+
+            $scope.delete = function () {
+                if (!$scope.active) return;
+
+                ViewService.delete($scope.active.id).then(function (result) {
+                    $scope.update();
+                });
+            };
+
+            $scope.save = function () {
+                if (!$scope.active) return;
+
+                if (!$scope.active.id) {
+                    ViewService.create($scope.active).then(function (result) {
+                        $scope.update();
+                    });
+
+                }
+                else {
+                    ViewService.update($scope.active).then(function (result) {
+                        $scope.update();
+                    });
+                }
+            };
+
+
+
+            /* $scope.toggle = function(group) {
+             var idx = $scope.active.groups.indexOf(group);
+
+             if (idx > -1) {
+             $scope.active.groups.splice(idx, 1);
+             }
+             else {
+             $scope.active.groups.push(group);
+             }
+             }
+
+             $scope.update();
+             }*/
+        }])
+.controller('ViewsEditController',
+    ['$scope', '$location','ViewService', 'GroupSetService', 'CrawlerService',
+        function ($scope, $location, ViewService, GroupSetService, CrawlerService) {
+            $scope.update = function () {
+                GroupSetService.getAll().then(function (data) {
+                    $scope.groups = data;
+                });
+                ViewService.getAll().then(function (data) {
+                    $scope.views = data;
+                    $scope.active = null;
+                });
+                CrawlerService.getAll().then(function (data) {
+                    $scope.crawlers = data;
+                    $scope.active = null;
+                })
+            };
+            $scope.create = function () {
+                $scope.active = {
+                    disabled: false,
+                    name: "",
+                    groups: [],
+                    crawlers: []
+                };
+            }
+            $scope.toggle = function (group) {
+                var idx = $scope.active.groups.indexOf(group);
+
+                if (idx > -1) {
+                    $scope.active.groups.splice(idx, 1);
+                }
+                else {
+                    $scope.active.groups.push(group);
+                }
+
+                $scope.update();
+            };
+            $scope.togglecrawlers = function (crawler) {
+                var idx = $scope.active.crawlers.indexOf(group);
+
+                if (idx > -1) {
+                    $scope.active.crawlers.splice(idx, 1);
+                }
+                else {
+                    $scope.active.crawlers.push(group);
+                }
+            }
+
+            $scope.update();
+
+        }]);
---
+++ de.fhbielefeld.scl.KINewsBoard.Web/web/admin/js/service.views.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Veronika on 12/1/2016.
+ */
+angular.module('nwbadmin')
+    .factory('ViewService', ['$http', '$q', function ($http, $q) {
+        var api = "/WebService/backend/view/"
+        var service = {};
+
+        service.getAll = function () {
+            return $http.get(api).then(function (response) {
+                return response.data;
+            });
+        };
+
+        service.get = function (id) {
+            return $http.get(api + id).then(function (response) {
+                return response.data;
+            });
+        };
+
+        service.create = function (entity) {
+            return $http.post(api, entity).then(function (response) {
+                return true;
+            });
+        }
+
+        service.update = function (entity) {
+            return $http.put(api, entity).then(function (response) {
+                return true;
+            });
+        }
+
+        service.delete = function (id) {
+            return $http.delete(api + id).then(function (response) {
+                return true;
+            });
+        }
+
+        return service;
+    }]);
