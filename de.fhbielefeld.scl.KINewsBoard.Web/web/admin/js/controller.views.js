a5af9c1a4a42698f0545d362c7ff93e18fbcee59
Cem Basoglu <cem.basoglu@web.de>
2016-12-17 09:03:59 +0100

    admin ui for views done.

--- de.fhbielefeld.scl.KINewsBoard.Web/web/admin/js/controller.views.js
+++ de.fhbielefeld.scl.KINewsBoard.Web/web/admin/js/controller.views.js
@@ -3,137 +3,115 @@
 angular.module('nwbadmin.views', ['ui.router'])
 
     .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-
         $stateProvider.state('views', {
             url: '/views',
+            abstract: true,
             templateUrl: 'views/views/index.html',
-            controller: 'ViewsViewController',
             data: {
                 authenticate: true,
             }
-        });
-        $stateProvider.state('editview', {
-            url: '/editviews',
+        }).state('views.list', {
+            url: '/',
+            templateUrl: 'views/views/list.html',
+            controller: 'ViewsViewController'
+        }).state('views.edit', {
+            url: '/edit',
+            params: {
+                view: undefined
+            },
             templateUrl: 'views/views/edit.html',
             controller: 'ViewsEditController'
         });
 
     }])
-    /*.controller('ViewsViewController',
-        ['$scope', '$location',
-            function ($scope, $location) {
+    .controller('ViewsViewController',
+        ['$scope', '$location', 'ViewService', '$state',
+            function ($scope, $location, ViewService, $state) {
 
-            }]);*/
-.controller('ViewsViewController',
-    ['$scope', '$location', 'ViewService', 'GroupSetService',
-        function ($scope, $location, ViewService, GroupSetService) {
-
-            $scope.inProgress = false;
-
-            $scope.update = function () {
-                GroupSetService.getAll().then(function (data) {
-                    $scope.groups = data;
-                });
-                ViewService.getAll().then(function (data) {
-                    $scope.views = data;
-                    $scope.active = null;
-                });
-            };
-
-            $scope.select = function (view) {
-                $scope.active = angular.copy(view);
-            };
-
-            $scope.delete = function () {
-                if (!$scope.active) return;
-
-                ViewService.delete($scope.active.id).then(function (result) {
-                    $scope.update();
-                });
-            };
-
-            $scope.save = function () {
-                if (!$scope.active) return;
-
-                $scope.inProgress = true;
-
-                var done = function (result) {
-                    if (result)
-                        $scope.update();
-
-                    $scope.inProgress = false;
-                }
-
-                if (!$scope.active.id)
-                    ViewService.create($scope.active).then(done);
-                else
-                    ViewService.update($scope.active).then(done);
-
-            };
-
-
-
-            /* $scope.toggle = function(group) {
-             var idx = $scope.active.groups.indexOf(group);
-
-             if (idx > -1) {
-             $scope.active.groups.splice(idx, 1);
-             }
-             else {
-             $scope.active.groups.push(group);
-             }
-             }
-
-             $scope.update();
-             }*/
-        }])
-.controller('ViewsEditController',
-    ['$scope', '$location','ViewService', 'GroupSetService', 'CrawlerService',
-        function ($scope, $location, ViewService, GroupSetService, CrawlerService) {
-            $scope.update = function () {
-                GroupSetService.getAll().then(function (data) {
-                    $scope.groups = data;
-                });
-                ViewService.getAll().then(function (data) {
-                    $scope.views = data;
-                    $scope.active = null;
-                });
-                CrawlerService.getAll().then(function (data) {
-                    $scope.crawlers = data;
-                    $scope.active = null;
-                })
-            };
-            $scope.create = function () {
-                $scope.active = {
-                    disabled: false,
-                    name: "",
-                    groups: [],
-                    crawlers: []
+                $scope.update = function () {
+                    ViewService.getAll().then(function (data) {
+                        $scope.views = data;
+                        $scope.active = null;
+                    });
                 };
-            }
-            $scope.toggle = function (group) {
-                var idx = $scope.active.groups.indexOf(group);
 
-                if (idx > -1) {
-                    $scope.active.groups.splice(idx, 1);
-                }
-                else {
-                    $scope.active.groups.push(group);
-                }
+                $scope.select = function (view) {
+                    $scope.active = view;
+                };
+
+                $scope.create = function () {
+                    $state.go("views.edit");
+                };
+
+                $scope.edit = function () {
+                    if (!$scope.active) return;
+
+                    $state.go("views.edit", {view: $scope.active});
+                };
+
+                $scope.delete = function () {
+                    if (!$scope.active) return;
+
+                    ViewService.delete($scope.active.id).then(function (result) {
+                        $scope.update();
+                    });
+                };
 
                 $scope.update();
-            };
-            $scope.togglecrawlers = function (crawler) {
-                var idx = $scope.active.crawlers.indexOf(group);
+            }])
+    .controller('ViewsEditController',
+        ['$scope', '$location', '$stateParams', 'ViewService', 'GroupSetService', 'CrawlerService', '$state',
+            function ($scope, $location, $stateParams, ViewService, GroupSetService, CrawlerService, $state) {
 
-                if (idx > -1) {
-                    $scope.active.crawlers.splice(idx, 1);
+                $scope.view = $stateParams.view;
+                $scope.inProgress = false;
+
+                GroupSetService.getAll().then(function (data) {
+                    $scope.groups = data;
+                });
+
+                CrawlerService.getAll().then(function (data) {
+                    $scope.crawlers = data;
+                });
+
+                if ($scope.view == null) {
+                    $scope.view = {
+                        disabled: false,
+                        name: "",
+                        description: "",
+                        groups: [],
+                        crawlers: []
+                    }
                 }
-                else {
-                    $scope.active.crawlers.push(group);
-                }
-            }
 
-            $scope.update();
+                $scope.toggle = function (list, item) {
+                    var idx = list.indexOf(item);
 
-        }]);
+                    if (idx > -1) {
+                        list.splice(idx, 1);
+                    }
+                    else {
+                        list.push(item);
+                    }
+                };
+
+                $scope.cancel = function () {
+                    $state.go("views.list");
+                };
+
+                $scope.save = function () {
+                    $scope.inProgress = true;
+
+                    var done = function (result) {
+                        if (result)
+                            $state.go("views.list");
+
+                        $scope.inProgress = false;
+                    };
+
+                    if (!$scope.view.id)
+                        ViewService.create($scope.view).then(done);
+                    else
+                        ViewService.update($scope.view).then(done);
+                };
+            }]);
