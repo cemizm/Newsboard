264293e1d878a326aca75e6cc5942e046b5f065e
Cem Basoglu <cem.basoglu@web.de>
2016-12-19 09:46:50 +0100

    simplified modal view

--- de.fhbielefeld.scl.KINewsBoard.Web/web/js/controller.frontend.js
+++ de.fhbielefeld.scl.KINewsBoard.Web/web/js/controller.frontend.js
@@ -9,6 +9,22 @@ angular.module('nwb.frontend', ['ui.router'])
                 url: '/?keyword&view',
                 templateUrl: 'views/frontend/index.html',
                 controller: 'FrontendViewController'
+            })
+            .state("frontend.detail", {
+                url: "detail/{news}/",
+                onEnter: ['$stateParams', '$state', '$uibModal', 'FrontendService', function ($stateParams, $state, $uibModal, FrontendService) {
+                    $uibModal.open({
+                        templateUrl: "views/frontend/detail.html",
+                        resolve: {
+                            news: function () {
+                                return FrontendService.getNewsEntryDetails($stateParams.news, $stateParams.view)
+                            }
+                        },
+                        controller: 'DetailViewController'
+                    }).result.finally(function () {
+                        $state.go('^');
+                    });
+                }]
             });
 
     }])
@@ -70,152 +86,33 @@ angular.module('nwb.frontend', ['ui.router'])
 
                 $scope.updateView();
 
-            }]);
+            }])
+    .controller('DetailViewController', ['$scope', 'news',
+        function ($scope, news) {
 
-angular.module('nwb.frontend').controller('ModalDemoCtrl', function ($uibModal, $log, $document, $sce, FrontendService) {
-    var $ctrl = this;
-    $ctrl.items = ['item1', 'item2', 'item3'];
+            $scope.entry = news;
+            $scope.selectedResult = null;
 
-    $ctrl.animationsEnabled = true;
-    $ctrl.colored = "";
-
-    $ctrl.getAnalyzerList = function () {
-        var analyzerArr = [];
-        $ctrl.entry.analyzerResults.forEach(function (analyzerResult) {
-            analyzerArr.push({
-                "id" : analyzerResult.analyzer.id,
-                "name" : analyzerResult.analyzer.name
-            });
-        });
-        return analyzerArr;
-    };
-
-    $ctrl.colorText = function (entry, analyzerIndex) {
-        var colored = "";
-
-        String.prototype.replaceBetween = function(start, end, what) {
-            return this.substring(0, start) + what + this.substring(end);
-        };
-
-        var currAnalyzer = -1;
-        var contentArr = entry.content.split('');
-        var contentArrIndex = 0;
-
-        contentArr.forEach(function (charIndex) {
-            if (currAnalyzer < 0) {
-
-            }
-            else {
-
-            }
-                contentArrIndex++;
-        });
-
-        entry.analyzerResults[analyzerIndex].sentenceResults.forEach(function (sentenceResult) {
-            var cssAttr = sentenceResult.value >= 0 ? "analyzerTextPos" : "analyzerTextNeg";
-            colored = entry.content.replaceBetween(sentenceResult.charStart, sentenceResult.charEnd, '<span class="' + cssAttr + '">' + entry.content.substring(sentenceResult.charStart, sentenceResult.charEnd) + '</span>');
-        });
-
-        return colored;
-
-    };
-
-
-    $ctrl.open = function (size, entry, parentSelector) {
-        var parentElem = parentSelector ?
-            angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
-
-        FrontendService.getNewsEntryDetails(entry.id).then(function (details) {
-            $ctrl.entry = details;
-            $ctrl.analyzerIndex = 0;
-            $ctrl.analyzerArr = $ctrl.getAnalyzerList();
-
-            $ctrl.colored = $ctrl.colorText(details, $ctrl.analyzerIndex);
-
-
-
-            var modalInstance = $uibModal.open({
-                animation: $ctrl.animationsEnabled,
-                ariaLabelledBy: 'modal-title',
-                ariaDescribedBy: 'modal-body',
-                templateUrl: 'myModalContent',
-                controller: 'ModalInstanceCtrl',
-                controllerAs: '$ctrl',
-                size: size,
-                appendTo: parentElem,
-                resolve: {
-                    items: function () {
-                        return $ctrl.items;
-                    },
-                    entry: function () {
-                        return $ctrl.entry;
-
-                    },
-                    colored: function () {
-                        return $ctrl.colored;
-                    }
-                }
-            });
-
-            modalInstance.result.then(function (selectedItem) {
-                $ctrl.selected = selectedItem;
-            }, function () {
-                $log.info('Modal dismissed at: ' + new Date());
-            });
-        });
-
-
-
-    };
-});
-
-// Please note that $uibModalInstance represents a modal window (instance) dependency.
-// It is not the same as the $uibModal service used above.
-
-angular.module('nwb.frontend').controller('ModalInstanceCtrl', function ($uibModalInstance, items, entry, colored) {
-    var $ctrl = this;
-    $ctrl.items = items;
-    $ctrl.entry = entry;
-    $ctrl.colored = colored;
-    $ctrl.selected = {
-        item: $ctrl.items[0]
-    };
-
-    $ctrl.ok = function () {
-        $uibModalInstance.close($ctrl.selected.item);
-    };
-
-    $ctrl.cancel = function () {
-        $uibModalInstance.dismiss('cancel');
-    };
-});
-
-// Please note that the close and dismiss bindings are from $uibModalInstance.
-
-angular.module('nwb.frontend').component('modalComponent', {
-    templateUrl: 'myModalContent',
-    bindings: {
-        resolve: '<',
-        close: '&',
-        dismiss: '&'
-    },
-    controller: function () {
-        var $ctrl = this;
-
-        $ctrl.$onInit = function () {
-            $ctrl.items = $ctrl.resolve.items;
-            $ctrl.selected = {
-                item: $ctrl.items[0]
+            $scope.dismiss = function () {
+                $scope.$dismiss();
             };
-            $ctrl.colored = $ctrl.resolve.colored;
-        };
 
-        $ctrl.ok = function () {
-            $ctrl.close({$value: $ctrl.selected.item});
-        };
+            $scope.updateText = function () {
+                $scope.colored = $scope.entry.content;
 
-        $ctrl.cancel = function () {
-            $ctrl.dismiss({$value: 'cancel'});
-        };
-    }
-});
+                if ($scope.selectedResult != null) {
+                    String.prototype.replaceBetween = function (start, end, what) {
+                        return this.substring(0, start) + what + this.substring(end);
+                    };
+
+                    $scope.selectedResult.sentenceResults.forEach(function (sentenceResult) {
+                        var cssAttr = sentenceResult.value >= 0 ? "analyzerTextPos" : "analyzerTextNeg";
+                        var subStr = $scope.colored.substring(sentenceResult.charStart, sentenceResult.charEnd);
+                        $scope.colored = $scope.colored.replaceBetween(sentenceResult.charStart,
+                            sentenceResult.charEnd, '<span class="' + cssAttr + '">' + subStr + '</span>');
+                    });
+                }
+            };
+
+            $scope.updateText()
+        }]);
